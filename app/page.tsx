fe39f1e5023583c870c20fe3287187e95dc6d7e7
Shane Morne Kolkoto <shanekolkoto@gmail.com>
2024-04-24 23:53:11 +0200

    last push for the day

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <About />
         <Skills />
         <Experience />
-        <Projects />
+        {/* <Projects /> */}
         <DiscordBot />
         <Footer />
       </div>
